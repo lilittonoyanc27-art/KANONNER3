8476d172252c26a8b96516623ff17c12fc199717
lilittonoyanc27-art <lilit.tonoyanc27@gmail.com>
2026-03-27 21:28:11 +0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TheoryApp from './TheoryApp';
+
+export default function App() {
+  return (
+    <div className="relative min-h-screen">
+      <TheoryApp />
+    </div>
+  );
+}
